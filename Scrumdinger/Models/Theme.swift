6ff7220ed7396150708e9c412adcce45bb004827
riku <riku.suny@gmail.com>
2022-03-03 19:05:47 +0900

    add detail

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -29,4 +29,8 @@ enum Theme: String {
     var mainColor: Color {
         Color(rawValue)
     }
+    
+    var name: String {
+        rawValue.capitalized
+    }
 }
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -7,15 +7,24 @@ struct ScrumsView: View {
         List {
             // scrumsはIdentifiableの配列なのでid:を指定する必要はない
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.theme.mainColor)
+                // NavigationLinkに渡すと自動でインジケータが追加される
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }.listRowBackground(scrum.theme.mainColor)
             }
+          // 子ビューに影響させることができる
+        }.navigationTitle("Daily Scrums").toolbar {
+            Button(action: {}) {
+                Image(systemName: "plus")
+            }.accessibilityLabel("New Scrum")
         }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
     }
 }
